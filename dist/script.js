f1afb9be36d8c90223c8176795aab3ea2829a233
Charles <voltprince098@gmail.com>
2021-06-24 00:48:59 +0700

    Create script.js

---
+++ dist/script.js
@@ -0,0 +1,42 @@
+<script type="text/javascript">
+$("#form-next-button, #form-prev-button").click(function() { $(window).scrollTop(0); });
+  var gpaInput = document.getElementById("client-address");
+  var gpaOutput = document.getElementById("clientaddress-text");
+  var aptInput = document.getElementById("aptcode");
+  var postal = document.querySelector('#postcode');
+	var autocomplete = new google.maps.places.Autocomplete(gpaInput, {
+    componentRestrictions: { country: ["us", "ca"] },
+    fields: ["address_components", "geometry"],
+    types: ["address"],
+  });
+  autocomplete.addListener("place_changed", filInAddress);
+
+function filInAddress() {
+  const place = autocomplete.getPlace();
+  let postcode = "";
+  for (const component of place.address_components) {
+    const componentType = component.types[0];
+    switch (componentType) {
+      case "postal_code": {
+        postcode = `${component.long_name}${postcode}`;
+        break;
+      }
+      case "postal_code_suffix": {
+        postcode = `${postcode}-${component.long_name}`;
+        break;
+      } } }
+  postal.value = postcode;
+}
+	const inputHandler = function(e) {
+  		document.getElementById('zipcode').innerHTML = e.target.value;
+	}
+  const myScript = function(e) {
+  		gpaOutput.innerHTML = gpaInput.value + " " + aptInput.value;
+	}
+  	gpaInput.addEventListener("focusout", myScript);
+    aptInput.addEventListener('input', myScript);
+		aptInput.addEventListener('propertychange', myScript);
+		postal.addEventListener('input', inputHandler);
+		postal.addEventListener('propertychange', inputHandler);
+</script>
+    
