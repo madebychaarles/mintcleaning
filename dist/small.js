e15531905690e03e0035c6fb9b2873b6426750c8
Charles <voltprince098@gmail.com>
2021-06-27 15:21:14 +0700

    Update small.js

--- dist/small.js
+++ dist/small.js
@@ -9,3 +9,13 @@ $('.mc-date-label').click(function(){
       $('#cleanhour-text').html($(select).val());
     });
   });
+
+$('#hidden-key').click(function(){
+	$('.mc-text-area.is--hiddenkey').show();
+    $('.mc-text-area.is--othergetin').hide();
+});
+
+$('#other-getin').click(function(){
+	$('.mc-text-area.is--othergetin').show();
+    $('.mc-text-area.is--hiddenkey').hide();
+});
